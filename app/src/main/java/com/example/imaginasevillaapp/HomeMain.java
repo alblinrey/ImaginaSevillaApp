91a63cf3975ae5d15c4cc3474aa3457c07a7c70c
alblinrey <mveebb@gmail.com>
2025-04-21 20:05:27 +0200

    ic launcher

--- app/src/main/java/com/example/imaginasevillaapp/HomeMain.java
+++ app/src/main/java/com/example/imaginasevillaapp/HomeMain.java
@@ -12,6 +12,7 @@ public class HomeMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_home);
